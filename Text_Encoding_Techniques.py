4c3631574c66d12acd494c8a7e6c905eb4b26a90
Jane <jenya.kissel@gmail.com>
2026-04-07 10:59:12 +0200

    Importing necessary libraries

--- Text_Encoding_Techniques.py
+++ Text_Encoding_Techniques.py
@@ -1,1 +1,17 @@
-# Importing dependencies
+# Importing Dependencies
+
+# Data Manipulation
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+
+# NLP Libraries
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Feature Extraction
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
